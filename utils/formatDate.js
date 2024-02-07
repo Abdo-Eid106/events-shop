cf258aefedf9db421faaf664bc9910fc61f8e91c
Abdo Eid <abdoeid2121@gmail.com>
2024-02-07 16:46:50 +0200

    fix bug in formatDate

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,8 +1,8 @@
-module.exports = (Date) => {
+module.exports = (date) => {
    const options = {
       year: "numeric",
       month: "long",
       day: "numeric",
    };
-   return new Intl.DateTimeFormat("en-US", options).format(Date);
+   return new Intl.DateTimeFormat("en-US", options).format(new Date(date));
 };
